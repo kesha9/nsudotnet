3a4f4a14e0f4bb28143272af1f6d5f35619524fb
Akerke Toktassyn <akerke.toktassyn@gmail.com>
2015-05-24 16:19:27 +0600

    Create Program.cs

---
+++ Toktassyn.Nsudotnet.LinesCounter/Toktassyn.Nsudotnet.LinesCounter/Program.cs
@@ -0,0 +1,77 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toktassyn.Nsudotnet.LinesCounter
+{
+    class Program
+    {
+           public static int Count(string directory, string[] typeOfFiles)
+        {
+            bool multiRowsComment = false;
+            string[] files = Directory.GetFiles(directory);
+            string[] directories = Directory.GetDirectories(directory);
+
+            int count = directories.Sum(dir => Count(dir, typeOfFiles));
+
+            foreach (string type in typeOfFiles)
+            {
+                foreach (string tr in files.Where(tr => tr.EndsWith(type)))
+                {
+                    using (StreamReader reader = new StreamReader(tr))
+                    {
+                        string str = null;
+                        while ((str = reader.ReadLine()) != null)
+                        {
+                            str = str.Trim();
+
+                            if (str.Contains("/*") && !str.StartsWith("/*"))
+                            {
+                                multiRowsComment = true;
+                                count++;
+                                continue;
+                            }
+
+                            if (str.Contains("*/") && !str.EndsWith("*/") && multiRowsComment)
+                            {
+                                multiRowsComment = false;
+                                count++;
+                                continue;
+                            }
+
+                            if (str.StartsWith("/*"))
+                            {
+                                multiRowsComment = true;
+                                continue;
+                            }
+
+                            if (str.StartsWith("*/") && multiRowsComment)
+                            {
+                                multiRowsComment = false;
+                                continue;
+                            }
+
+                            if (str.StartsWith("//") || multiRowsComment || str.Length == 0)
+                            {
+                                continue;
+                            }
+
+                            count++;
+                        } 
+                    }
+                }
+            }
+            return count;
+        }
+            
+            static void Main(string[] args)
+        {
+            string curdir = args[0];
+            int result = Count(curdir, args.Skip(1).ToArray());
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+    }
